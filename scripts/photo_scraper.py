b68098460040bc298af22328d317f9edbfdbb709
getahunTiruneh <gechtiru@gmail.com>
2024-10-11 23:10:23 +0300

    update scraper script

--- scripts/photo_scraper.py
+++ scripts/photo_scraper.py
@@ -1,20 +1,29 @@
 import os
 import logging
+from telethon.tl.types import MessageMediaPhoto
 
-# Function to download media (photos) and save in the folder
+# Function to download media (photos) from a message and save them to a folder
 async def download_images(message, channel_name, client):
+    """
+    Download media (photos) from a Telegram message and save them to a specified folder.
+
+    Args:
+        message: The Telegram message containing media.
+        channel_name: The name of the channel from which the image is downloaded.
+        client: The Telegram client used to interact with the Telegram API.
+    """
     try:
         # Create folder if it doesn't exist
-        folder_path = 'photos/'
+        folder_path = '../data/photos/'
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
-        # If message contains media and it's a photo, download it
-        if message.media and 'image' in message.media.document.mime_type:
-            file_path = os.path.join(folder_path, f"{message.id}.jpg")
-            await client.download_media(message, file_path)
+        # Check if the message contains a photo
+        if isinstance(message.media, MessageMediaPhoto):
+            file_path = os.path.join(folder_path, f"{channel_name}_{message.id}.jpg")
+            await client.download_media(message.media, file_path)
             logging.info(f"Downloaded image from message {message.id} in {channel_name}")
-            return file_path
-        return None
+        else:
+            logging.info(f"No valid image found in message {message.id}.")
     except Exception as e:
         logging.error(f"Error downloading image from message {message.id}: {e}")
