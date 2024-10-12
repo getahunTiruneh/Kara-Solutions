7bfdae5acb59c1c93f75f65d15d7217b808f9008
getahunTiruneh <gechtiru@gmail.com>
2024-10-12 15:50:56 +0300

    fix unittest error

--- database/database_conn.py
+++ database/database_conn.py
@@ -1,6 +1,10 @@
 import psycopg2
 import os
 import logging
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
